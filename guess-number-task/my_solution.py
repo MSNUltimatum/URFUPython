8795fda15bc640fac6d3fda49aea714760bc9495
MSNUltimatum <egrsar2000@gmail.com>
2022-09-27 19:57:36 +0400

    Added to jupyter

--- guess-number-task/my_solution.py
+++ guess-number-task/my_solution.py
@@ -1,10 +1,10 @@
 import numpy as np
+from game_v2 import score_game
 
 
 class Randomizer:
     def __init__(self):
         self.number = np.random.randint(1, 101)
-        print(f"Random number is: {self.number}")
 
     def equals(self, predict: int) -> bool:
         return self.number == predict
@@ -13,22 +13,16 @@ class Randomizer:
         return self.number > predict
 
 
-def predict_number(rnd: Randomizer) -> int:
+def predict_number(num: int) -> int:
     start: int = 1
-    end: int = 100
+    end: int = 101
     attempts: int = 0
-    while start <= end:
+    while start < end:
         attempts += 1
         mid: int = (start + end) // 2
-        if rnd.equals(mid):
-            print(f"Attempts count: {attempts}")
-            return mid
-        elif rnd.higher(mid):
+        if num == mid:
+            return attempts
+        elif num > mid:
             start: int = mid
         else:
             end = mid
-
-
-if __name__ == '__main__':
-    rand: Randomizer = Randomizer()
-    print(f"Result number: {predict_number(rand)}")
